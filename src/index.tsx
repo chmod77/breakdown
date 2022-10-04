5814a88834616fcce2ec2682ae8728f37a5c670b
chmod77 <chmod777.ke@gmail.com>
2022-10-04 08:18:13 +0300

    feat: switch to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import 'antd/dist/antd.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(
