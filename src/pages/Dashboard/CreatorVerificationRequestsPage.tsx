b4abd37e408b7cdb08f7b69195a8f3c1bb2144cc
chmod77 <chmod777.ke@gmail.com>
2023-03-02 09:24:36 +0300

    chore: add all changes for handover

--- src/pages/Dashboard/CreatorVerificationRequestsPage.tsx
+++ src/pages/Dashboard/CreatorVerificationRequestsPage.tsx
@@ -3,6 +3,9 @@ import { getVerificationRequests } from '../../services/rest'
 import { Table } from '../sections'
 import RootPage from './RootPage'
 
+//@ts-ignore
+import { useTable, useSortBy } from 'react-table'
+
 export default function CreatorVerificationRequestsPage() {
   const [requests, setRequests] = useState<null | any | []>([])
 
@@ -10,62 +13,215 @@ export default function CreatorVerificationRequestsPage() {
     let verificationRequests = await getVerificationRequests()
     if (verificationRequests?.success) {
       setRequests(verificationRequests?.requests)
+
     }
   }
+  const data = React.useMemo(() => requests, [requests])
 
+  /**
+   * {
+            "id": 2,
+            "user_id": 2,
+            "known_as": "Tester",
+            "channel_id": "2",
+            "tier": 1,
+            "status": 0,
+            "proof_of_work": "www.me.co",
+            "video": "https://breakdown-bucket.s3.amazonaws.com/verifications/lSxdM6iOp8oeRWdy9eBJ6TMkEvB8gQ0AlsN5WFv9.mp4",
+            "photo_id": "https://breakdown-bucket.s3.amazonaws.com/verifications/cxSRngZ95rBTkCi6O0kRDjwuWaOFsoQyiP2PLlV1.jpg",
+            "created_at": "2022-09-08T16:20:26.000000Z",
+            "updated_at": "2022-10-06T23:16:04.000000Z"
+        }
+   */
+  const columns = React.useMemo(
+    () => [
+
+      {
+        Header: 'Tier Type',
+        accessor: 'tier',
+      },
+      {
+        Header: "Category Affiliations",
+        accessor: 'channel_id'
+      },
+      {
+        Header: "Date",
+        accessor: 'created_at'
+      },
+      {
+        Header: "Action"
+      }
+    ],
+    []
+  )
+
+  // const {
+  //   getTableProps,
+  //   getTableBodyProps,
+  //   headerGroups,
+  //   rows,
+  //   prepareRow,
+  //   page,
+  //   canPreviousPage,
+  //   canNextPage,
+  //   pageOptions,
+  //   pageCount,
+  //   gotoPage,
+  //   nextPage,
+  //   previousPage,
+  //   setPageSize,
+  //   state: { pageIndex, pageSize },
+
+  // }: any  = useTable({ columns, data, initialState: { pageIndex: 0 }, usePagination})
+  const tableInstance = useTable({ columns, data }, useSortBy)
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  }: any = useTable({ columns, data },
+    useSortBy,
+    (hooks: any) => {
+      hooks.visibleColumns.push((columns: any) => [
+        {
+          Header: "User",
+          id: 'view',
+          Cell: ({ row }: any) => (
+            <div onClick={() => console.log(row)}>
+
+              {
+                row.original.photo_id ? (
+                  <>
+                    <img src={row.original.photo_id} style={{
+                      height: 40, width: 40, borderRadius: 50
+                    }} />
+                    <span className="align-middle" style={{
+                      marginLeft: 10
+                    }}>{row.original.known_as}</span>
+                  </>
+                ) : (
+                  <>
+                    <img src={"assets/images/user-vector.png"} style={{
+                      height: 40, width: 40, borderRadius: 50
+                    }} />
+                    <span className="align-middle" style={{
+                      marginLeft: 10
+                    }}>{row.original.known_as}</span>
+
+                  </>
+
+                )
+
+              }
+
+
+            </div>
+          )
+        },
+
+        ...columns,
+
+      ])
+
+    }
+
+  )
   useEffect(() => {
     onLoad()
   }, [])
   return (
     <RootPage>
 
+
+
       <Table title={"Creator Verification Requests"}>
-        <table className="table align-middle table-nowrap mb-0">
+
+        <table {...getTableProps()} className="table align-middle table-nowrap mb-0">
           <thead>
-            <tr>
-              <th scope="col" style={{
-                fontFamily: "Roboto"
-              }}>User</th>
-              <th scope="col" style={{
-                fontFamily: "Roboto"
-              }}>Tier Type</th>
-              <th scope="col" style={{
-                fontFamily: "Roboto"
-              }}>Category Affiliations</th>
-              <th scope="col" style={{
-                fontFamily: "Roboto"
-              }}>Date</th>
-              <th scope="col" style={{
-                fontFamily: "Roboto"
-              }}>Action</th>
-            </tr>
+
+            {headerGroups.map((headerGroup: any) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column: any) => (
+                  <th
+                    {...column.getHeaderProps()}
+                    scope="col" style={{
+                      fontFamily: "Roboto"
+                    }}
+                  >
+                    {column.render('Header')}
+                  </th>
+                ))}
+              </tr>
+            ))}
           </thead>
-          <tbody>
-            {
-              requests?.length > 0 ? (
-                requests.map((request: any, index: any) => (
-                  <tr>
-                    <th scope="row"><a href="#" className="fw-medium"><img className="rounded-circle header-profile-user" src={`${request.photo_id == null ? 'assets/images/user-vector.png' : request.photo_id}`} alt={`${request.known_as}`} style={{
-                      marginRight: 5
-                    }} />{request.known_as}</a></th>
-                    <td><span className="badge badge-lg badge-pill bg-warning" style={{
-                      fontSize: 14,
-                      borderRadius: 20,
-                      color: "#000"
-                    }} data-key="t-new">Tier {request.tier}</span></td>
-                    <td>TV</td>
-                    <td>{`${new Date(request.created_at).toDateString()}`}</td>
-                    <td><a href="javascript:void(0);" className="link-success">View More <i className="ri-eye-line align-middle" /></a></td>
-                  </tr>
-                ))
-              ) : (
-                null
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row: any) => {
+              prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell: any) => {
+                    return (
+                      <td
+                        {...cell.getCellProps()}
+
+                      >
+                        {cell.render('Cell')}
+                      </td>
+                    )
+                  })}
+                </tr>
               )
-            }
-
-
+            })}
           </tbody>
         </table>
+        {/* 
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div> */}
+
+
       </Table>
     </RootPage>
   )
